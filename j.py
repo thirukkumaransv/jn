d3e58639244538ef3ec86686121e32f9d3d44fa7
thirukkumaransv <52705136+thirukkumaransv@users.noreply.github.com>
2019-07-15 09:57:05 -0700

    Update j.py

--- j.py
+++ j.py
@@ -1,5 +1,5 @@
-v1,s2=map(int,input().split())
+v,s=map(int,input().split())
 c3=list(map(int,input().split()))
-for j in range (0,s2):
+for j in range (0,s):
     c3=[c3[-1]]+c3[:-1]
 print(*c3,end='')
